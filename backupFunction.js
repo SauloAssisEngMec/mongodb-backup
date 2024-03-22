ed481242bf913758a74cc74a507b9d6ff890d83e
saulo <sauloassis4123@gmail.com>
2024-03-22 12:04:43 -0300

    feat: backupFunction created

---
+++ backupFunction.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const fs = require("fs");
+const AWS = require("aws-sdk");
+const exec = require("./execFunction.js");
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+});
+
+const uri = process.env.NEW_DATABASE_URI || process.env.URI;
+const backupName = process.env.BACKUP_NAME;
+const bucket = process.env.BUCKET;
+
+(async () => {
+  const s3 = new AWS.S3();
+  const __dirname = path.resolve();
+
+  const dumpPath = path.resolve(__dirname, backupName);
+
+  const command = `mongodump --uri '${uri}' --gzip --archive=${dumpPath}`;
+
+  try {
+    await exec(command);
+
+    const readStream = fs.createReadStream(dumpPath);
+    const params = {
+      Bucket: bucket,
+      Key: backupName,
+      Body: readStream,
+    };
+
+    await s3.putObject(params).promise();
+
+    console.log("Successful backup to s3!");
+  } catch (err) {
+    console.log(`Backup failed: ${err}`);
+  }
+})();
